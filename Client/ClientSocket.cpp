4786804a08d2c964a91041d53b5b20599120dfbb
TVM <thaivanmanh2004@gmail.com>
2023-12-06 11:05:24 +0700

    TVM 06/12/2023

--- Client/ClientSocket.cpp
+++ Client/ClientSocket.cpp
@@ -16,117 +16,33 @@
 
 void CClientSocket::OnReceive(int nErrorCode)
 {
-	// local variables used in file transfer (declared here to avoid
-	// "goto skips definition"-style compiler errors)
+    int imageSize;
+    this->Receive(&imageSize, sizeof(int)); // Nhận kích thước dữ liệu ảnh
 
-	BOOL bRet = TRUE; // return value
-	// used to monitor the progress of a receive operation
-	int dataLength, cbBytesRet, cbLeftToReceive;
-	// pointer to buffer for receiving data
-	// (memory is allocated after obtaining file size)
-	BYTE* recdData = NULL;
+    BITMAPINFOHEADER bmpInfoHeader;
+    this->Receive(&bmpInfoHeader, sizeof(BITMAPINFOHEADER)); // Nhận thông tin header của bitmap
 
-	CFile destFile;
-	CFileException fe;
-	BOOL bFileIsOpen = FALSE;
+    int width = bmpInfoHeader.biWidth;
+    int height = abs(bmpInfoHeader.biHeight);
 
-	// open/create target file that receives the transferred data
+    // Nhận dữ liệu ảnh
+    BYTE* bitmapData = new BYTE[imageSize];
+    this->Receive(bitmapData, imageSize);
 
-	if (!(bFileIsOpen = destFile.Open(L"screenshot1.bmp", CFile::modeCreate |
-		CFile::modeWrite | CFile::typeBinary, &fe)))
-	{
-		TCHAR strCause[256];
-		fe.GetErrorMessage(strCause, 255);
-		TRACE("GetFileFromRemoteSender encountered an error while opening the local file\n"
-			"\tFile name = %s\n\tCause = %s\n\tm_cause = %d\n\tm_IOsError = %d\n",
-			fe.m_strFileName, strCause, fe.m_cause, fe.m_lOsError);
+    // Hiển thị ảnh lên màn hình
+    HDC hDC = GetDC(NULL);
+    HDC hMemDC = CreateCompatibleDC(hDC);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hDC, width, height);
 
-		/* you should handle the error here */
+    SetDIBits(hMemDC, hBitmap, 0, height, bitmapData + sizeof(BITMAPINFOHEADER), (BITMAPINFO*)&bmpInfoHeader, DIB_RGB_COLORS);
 
-		bRet = FALSE;
-		goto PreReturnCleanup;
-	}
+    StretchBlt(hDC, 0, 0, width, height, hMemDC, 0, 0, width, height, SRCCOPY);
 
-	// get the file's size first
-	cbLeftToReceive = sizeof(dataLength);
-
-	do
-	{
-		BYTE* bp = (BYTE*)(&dataLength) + sizeof(dataLength) - cbLeftToReceive;
-		cbBytesRet = ((CClientApp*)AfxGetApp())->m_ClientSocket.Receive(bp, cbLeftToReceive);
-
-		// test for errors and get out if they occurred
-		if (cbBytesRet == SOCKET_ERROR || cbBytesRet == 0)
-		{
-			int iErr = ::GetLastError();
-			TRACE("GetFileFromRemoteSite returned a socket error while getting file length\n"
-				"\tNumber of Bytes received (zero means connection was closed) = %d\n"
-				"\tGetLastError = %d\n", cbBytesRet, iErr);
-
-			/* you should handle the error here */
-
-			bRet = FALSE;
-			goto PreReturnCleanup;
-		}
-
-		// good data was retrieved, so accumulate
-		// it with already-received data
-		cbLeftToReceive -= cbBytesRet;
-
-	} while (cbLeftToReceive > 0);
-
-	dataLength = ntohl(dataLength);
-
-	// now get the file in RECV_BUFFER_SIZE chunks at a time
-
-	recdData = new byte[RECV_BUFFER_SIZE];
-	cbLeftToReceive = dataLength;
-
-	do
-	{
-		int iiGet, iiRecd;
-
-		iiGet = (cbLeftToReceive < RECV_BUFFER_SIZE) ?
-			cbLeftToReceive : RECV_BUFFER_SIZE;
-		iiRecd = ((CClientApp*)AfxGetApp())->m_ClientSocket.Receive(recdData, iiGet);
-
-		// test for errors and get out if they occurred
-		if (iiRecd == SOCKET_ERROR || iiRecd == 0)
-		{
-			int iErr = ::GetLastError();
-			TRACE("GetFileFromRemoteSite returned a socket error while getting chunked file data\n"
-				"\tNumber of Bytes received (zero means connection was closed) = %d\n"
-				"\tGetLastError = %d\n", iiRecd, iErr);
-
-			/* you should handle the error here */
-
-			bRet = FALSE;
-			goto PreReturnCleanup;
-		}
-
-		// good data was retrieved, so accumulate
-		// it with already-received data
-
-		destFile.Write(recdData, iiRecd); // Write it
-		cbLeftToReceive -= iiRecd;
-
-	} while (cbLeftToReceive > 0);
-	
-PreReturnCleanup: // labelled "goto" destination
-
-	// free allocated memory
-	// if we got here from a goto that skipped allocation,
-	// delete of NULL pointer
-	// is permissible under C++ standard and is harmless
-	delete[] recdData;
-
-	if (bFileIsOpen) {
-		destFile.Close();
-		bFileIsOpen = false;
-	}
-	if (!bFileIsOpen) {
-		((CMainFrame*)AfxGetMainWnd())->OnDisplayImage();
-	}
+    // Giải phóng tài nguyên
+    DeleteObject(hBitmap);
+    DeleteDC(hMemDC);
+    ReleaseDC(NULL, hDC);
+    delete[] bitmapData;
 	((CClientApp*)AfxGetApp())->m_ClientSocket.Send("TVM", 3);
 	// only close file if it's open (open might have failed above)
 
--- Server/ServerSocket.cpp
+++ Server/ServerSocket.cpp
@@ -13,169 +13,48 @@ void CServerSocket::OnReceive(int nErrorCode)
     char buff[3];
     Receive(buff, 3);
 
-    // Get the dimensions of the primary display
-    DEVMODE dm = { 0 };
-    dm.dmSize = sizeof(dm);
-    EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &dm);
-    int width = dm.dmPelsWidth;
-    int height = dm.dmPelsHeight;
+    HDC hScreenDC = GetDC(NULL);
+    HDC hMemDC = CreateCompatibleDC(hScreenDC);
 
-    // Create a device context for the primary display
-    HDC hScreen = GetDC(NULL);
-    HDC hDC = CreateCompatibleDC(hScreen);
+    int width = GetSystemMetrics(SM_CXSCREEN);
+    int height = GetSystemMetrics(SM_CYSCREEN);
 
-    // Create a bitmap compatible with the primary display
-    HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, width, height);
-    HBITMAP hOldBitmap = (HBITMAP)SelectObject(hDC, hBitmap);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, width, height);
+    HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemDC, hBitmap);
 
-    // Copy the contents of the primary display to the bitmap
-    BitBlt(hDC, 0, 0, width, height, hScreen, 0, 0, SRCCOPY);
+    BitBlt(hMemDC, 0, 0, width, height, hScreenDC, 0, 0, SRCCOPY);
 
-    // Save the bitmap to a BMP file
-    const char* filename = "screenshot.bmp";
-    BITMAPINFOHEADER bmpInfoHeader = { 0 };
+    BITMAPINFOHEADER bmpInfoHeader;
+    memset(&bmpInfoHeader, 0, sizeof(BITMAPINFOHEADER));
     bmpInfoHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bmpInfoHeader.biWidth = width;
-    bmpInfoHeader.biHeight = -height;
     bmpInfoHeader.biPlanes = 1;
     bmpInfoHeader.biBitCount = 24;
     bmpInfoHeader.biCompression = BI_RGB;
-    bmpInfoHeader.biSizeImage = ((width * bmpInfoHeader.biBitCount + 31) / 32) * 4 * height;
+    bmpInfoHeader.biWidth = width;
+    bmpInfoHeader.biHeight = -height; // negative height to indicate top-down DIB
+    bmpInfoHeader.biSizeImage = ((width * 24 + 31) / 32) * 4 * height;
 
-    BITMAPFILEHEADER bmpFileHeader = { 0 };
-    bmpFileHeader.bfType = 'MB';
-    bmpFileHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-    bmpFileHeader.bfSize = bmpFileHeader.bfOffBits + bmpInfoHeader.biSizeImage;
+    // Calculate size of image data
+    int imageSize = bmpInfoHeader.biSizeImage + sizeof(BITMAPINFOHEADER);
 
-    std::ofstream outputFile(filename, std::ios::out | std::ios::binary);
-    outputFile.write(reinterpret_cast<const char*>(&bmpFileHeader), sizeof(BITMAPFILEHEADER));
-    outputFile.write(reinterpret_cast<const char*>(&bmpInfoHeader), sizeof(BITMAPINFOHEADER));
+    // Send image size first
+    this->Send(&imageSize, sizeof(int));
 
-    BYTE* pixels = new BYTE[bmpInfoHeader.biSizeImage];
-    GetDIBits(hDC, hBitmap, 0, height, pixels, reinterpret_cast<BITMAPINFO*>(&bmpInfoHeader), DIB_RGB_COLORS);
-    outputFile.write(reinterpret_cast<const char*>(pixels), bmpInfoHeader.biSizeImage);
+    // Send bitmap info header
+    this->Send(&bmpInfoHeader, sizeof(BITMAPINFOHEADER));
 
-    // Clean up
-    outputFile.close();
-    delete[] pixels;
-    SelectObject(hDC, hOldBitmap);
+    // Get DIB bits
+    BYTE* bitmapData = new BYTE[imageSize];
+    GetDIBits(hMemDC, hBitmap, 0, height, bitmapData + sizeof(BITMAPINFOHEADER), (BITMAPINFO*)&bmpInfoHeader, DIB_RGB_COLORS);
+
+    // Send bitmap data
+    this->Send(bitmapData, imageSize);
+
+    // Cleanup
+    delete[] bitmapData;
+    SelectObject(hMemDC, hOldBitmap);
     DeleteObject(hBitmap);
-    DeleteDC(hDC);
-    ReleaseDC(NULL, hScreen);
-
-
-
-    // return value
-    BOOL bRet = TRUE;
-    // used to monitor the progress of a sending operation
-    int fileLength, cbLeftToSend;
-    // pointer to buffer for sending data
-    // (memory is allocated after sending file size)
-    BYTE* sendData = NULL;
-
-    CFile sourceFile;
-    CFileException fe;
-    BOOL bFileIsOpen = FALSE;
-
-    if (!(bFileIsOpen = sourceFile.Open(L"screenshot.bmp",
-        CFile::modeRead | CFile::typeBinary, &fe)))
-    {
-        TCHAR strCause[256];
-        fe.GetErrorMessage(strCause, 255);
-        TRACE("SendFileToRemoteRecipient encountered an error while opening the local file\n"
-            "\tFile name = %s\n\tCause = %s\n\tm_cause = %d\n\tm_IOsError = %d\n",
-            fe.m_strFileName, strCause, fe.m_cause, fe.m_lOsError);
-
-        /* you should handle the error here */
-
-        bRet = FALSE;
-        goto PreReturnCleanup;
-    }
-
-    // first send length of file
-
-    fileLength = sourceFile.GetLength();
-    fileLength = htonl(fileLength);
-
-    cbLeftToSend = sizeof(fileLength);
-
-    do
-    {
-        int cbBytesSent;
-        BYTE* bp = (BYTE*)(&fileLength) + sizeof(fileLength) - cbLeftToSend;
-        cbBytesSent = this->Send(bp, cbLeftToSend);
-
-        // test for errors and get out if they occurred
-        if (cbBytesSent == SOCKET_ERROR)
-        {
-            int iErr = ::GetLastError();
-            TRACE("SendFileToRemoteRecipient returned a socket error while sending file length\n"
-                "\tNumber of Bytes sent = %d\n"
-                "\tGetLastError = %d\n", cbBytesSent, iErr);
-
-            /* you should handle the error here */
-
-            bRet = FALSE;
-            goto PreReturnCleanup;
-        }
-
-        // data was successfully sent, so account
-        // for it with already-sent data
-        cbLeftToSend -= cbBytesSent;
-    } while (cbLeftToSend > 0);
-
-
-    // now send the file's data    
-    sendData = new BYTE[SEND_BUFFER_SIZE];
-
-    cbLeftToSend = sourceFile.GetLength();
-
-    do
-    {
-        // read next chunk of SEND_BUFFER_SIZE bytes from file
-
-        int sendThisTime, doneSoFar, buffOffset;
-
-        sendThisTime = sourceFile.Read(sendData, SEND_BUFFER_SIZE);
-        buffOffset = 0;
-
-        do
-        {
-            doneSoFar = this->Send(sendData + buffOffset,
-                sendThisTime);
-
-            // test for errors and get out if they occurred
-            if (doneSoFar == SOCKET_ERROR)
-            {
-                int iErr = ::GetLastError();
-                TRACE("SendFileToRemoteRecipient returned a socket error  while sending chunked file data\n"
-                    "\tNumber of Bytes sent = %d\n"
-                    "\tGetLastError = %d\n", doneSoFar, iErr);
-
-                /* you should handle the error here */
-
-                bRet = FALSE;
-                goto PreReturnCleanup;
-            }
-
-            // data was successfully sent,
-            // so account for it with already-sent data
-
-            buffOffset += doneSoFar;
-            sendThisTime -= doneSoFar;
-            cbLeftToSend -= doneSoFar;
-        } while (sendThisTime > 0);
-
-    } while (cbLeftToSend > 0);
-
-PreReturnCleanup: // labelled goto destination
-
-    delete[] sendData;
-
-    if (bFileIsOpen)
-        sourceFile.Close();
-
-
-
+    DeleteDC(hMemDC);
+    ReleaseDC(NULL, hScreenDC);
     CSocket::OnReceive(nErrorCode);
 }
